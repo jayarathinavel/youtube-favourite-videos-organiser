54d3c56a6a307473636b031aeb5790eaa14f92c5
jayarathinavel <jayrathinavel@gmail.com>
2021-12-17 14:10:54 +0530

    Nav Bar Dropdown Changes

--- nav-bar.php
+++ nav-bar.php
@@ -3,7 +3,7 @@
     $username = $_SESSION["username"];
 
     echo '<div id="navbar">';
-    echo '<a href="/index.php" id="home">Home</a>';
+    echo '<a href="/index.php" class="navbar-a" id="home">Home</a>';
 
     echo '<select id="tags">';
     echo '<option> Select a tag </option>';
@@ -25,14 +25,19 @@
         <div onclick="myFunction()" class="dropdown" style="float:right;" >
         <div id="user">'.$username.'</div>
             <div id="myDropdown" class="dropdown-content">
-            2<br>
-                3
+            <a href="./dashboard.php" class="dropdown-links" > Dashboard </a>
+            <a href="./logout.php" class="dropdown-links" > Logout </a>
             </div>
         </div>
         ';
     }
+    else{
+        echo '        
+        <a href="./login.php" class="navbar-a" style="float:right;" id ="user" > Login </a>';
+    }
+
     echo '        
-    <a href="/insert/form.html" style="float:right;" id ="insert" > Insert </a>
-    <a href="/index.php?editMode=true" style="float:right;" id="edit"> Edit </a>
+    <a href="/insert/form.html" class="navbar-a" style="float:right;" id ="insert" > Insert </a>
+    <a href="/index.php?editMode=true" class="navbar-a" style="float:right;" id="edit"> Edit </a>
     </div>';
 ?>
