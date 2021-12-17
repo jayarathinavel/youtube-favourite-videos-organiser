3257770015a2c3fe8277819cbea286618904e85a
jayarathinavel <jayrathinavel@gmail.com>
2021-12-17 13:20:44 +0530

    Database Credentials from Environment Variables

--- database.php
+++ database.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "coolbuddy";
-    $dbname = "hobby";
+    $servername = getenv('DB_HOST');
+    $username = getenv('DB_USERNAME');
+    $password = getenv('DB_PASSWORD');
+    $dbname = getenv('DB_NAME');
   
   $conn = mysqli_connect($servername, $username, $password, $dbname);
   if ($conn->connect_error) {
