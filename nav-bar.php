36bef666610b663f502bf11b1c0c84d352e7e2f1
jayarathinavel <jayrathinavel@gmail.com>
2021-12-17 22:43:55 +0530

    Tags Dropdown

--- nav-bar.php
+++ nav-bar.php
@@ -9,25 +9,39 @@
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-          <li class="nav-item btn btn-sm btn-secondary ms-2 me-2">
-            <a class="nav-link active " href="/index.php">Home</a>
+      <ul class="navbar-nav me-auto mb-1 mb-lg-0">
+          <li class="nav-item m-1">
+            <a class=" btn btn-sm btn-secondary nav-link" href="/index.php">Home</a>
+          </li> 
+          <li class="nav-item m-1">
+            <a class="btn btn-sm btn-secondary nav-link" href="/insert/form.html">Insert</a>
           </li>
-          <li class="nav-item btn btn-sm btn-secondary ms-2 me-2">
-            <a class="nav-link" href="/insert/form.html">Insert</a>
+          <li class="nav-item m-1">
+            <a class="btn btn-sm btn-secondary nav-link" href="/index.php?editMode=true">Edit Mode</a>
           </li>
-          <li class="nav-item btn btn-sm btn-secondary ms-2 me-2">
-            <a class="nav-link" href="/index.php?editMode=true">Edit Mode</a>
-          </li>
-          <li class="nav-item btn btn-sm btn-secondary ms-2 me-2">
-            <a class="nav-link" href="#">Tags</a>
+          <li class="nav-item dropdown m-1">
+            <a class="btn btn-sm btn-secondary nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> Tags </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <?php
+                for ($i = 0; $i < count($tagsSingle); $i++) {
+                    if ($tagsUnique[$i]) { //To filter the empty array values
+                        if($tagsUnique[$i] == $_GET['tagName']){ //For Default value in Select
+                        echo '<li><a class="dropdown-item" href="/index.php?isTagSelected=true&tagName=' . $tagsUnique[$i] . '">' . $tagsUnique[$i] . '</a></li>';
+                        }
+                        else{
+                        echo '<li><a class="dropdown-item" href="/index.php?isTagSelected=true&tagName=' . $tagsUnique[$i] . '">' . $tagsUnique[$i] . '</a></li>';
+                        }
+                    }
+                }
+              ?>
+            </ul>
           </li>
       </ul>
       <?php
         if($isLoggedIn){ echo '
           <ul class="navbar-nav">
-              <li class="nav-item dropdown btn btn-secondary">
-                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> '.$username.' </a>
+              <li class="nav-item dropdown">
+                  <a class="btn btn-sm btn-secondary nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"> '.$username.' </a>
                   <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                     <li><a class="dropdown-item" href="./dashboard.php">Dashboard</a></li>
                     <li><a class="dropdown-item" href="./logout.php">Logout</a></li>
